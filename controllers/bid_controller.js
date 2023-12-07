36215d33dab94c15fc0fe405d5da085e07121ca2
philiphart1006 <philiphart1006@gmail.com>
2023-12-07 15:57:58 +0000

    adds bid controller; bid/review routes; updated bundle schema; added seeding for bids/reviews

---
+++ controllers/bid_controller.js
@@ -0,0 +1,88 @@
+import Bid from '../models/bid_model.js'
+
+// & Index
+// ? GET
+// * /bids
+
+export const getAllBids = async (req, res) => {
+  const bids = await Bid.find()
+  return res.json(bids)
+}
+
+// & Create
+// ? POST
+// * /bids
+
+export const createBid = async (req, res) => {
+  try {
+    req.body.owner = req.currentUser._id
+    const bidToCreate = await Bid.create(req.body)
+    return res.status(201).json(bidToCreate)
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json(error)
+  }
+}
+
+// & Get One
+// ? GET
+// * /bids/:bidId
+
+export const getSingleBid = async (req, res) => {
+  try {
+    const { bidId } = req.params
+    const bid = await Bid.findById(bidId).populate('owner')
+    if (!bid){
+      return res.status(404).json({ message: 'Bid not found' })
+    }
+    // ? If the bid exists
+    return res.json(bid)
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json(error)
+  }
+}
+
+// & Update
+// ? PUT
+// * /bids/:bidId
+
+export const updateBid = async (req, res) => {
+  try {
+    const { bidId } = req.params
+    const bid = await Bid.findById(bidId)
+    if (!bid) {
+      return res.status(404).json({ message: 'Bid not found ' })
+    }
+    console.log('User making request: ', req.currentUser._id)
+    console.log('User that owns bid: ', bid.owner)
+    console.log('Does user match owner: ', bid.owner.equals(req.currentUser._id))
+    if (!bid.owner.equals(req.currentUser._id)){
+      return res.status(401).json({ message: 'Unauthorized' })
+    }
+    Object.assign(bid, req.body)
+    await bid.save()
+    return res.json(bid) 
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json(error)
+  }
+}
+
+// & Delete
+// ? DELETE
+// * /bids/:bidId
+
+export const deleteBid = async (req, res) => {
+  try {
+    const { bidId } = req.params
+    const bid = await Bid.findOneAndDelete({ _id: bidId, owner: req.currentUser._id })
+    if (!bid) {
+      return res.status(404).json({ message: 'Bid not found : unauthorized' })
+    }
+    return res.sendStatus(204)
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json(error)
+  }
+}
