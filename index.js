ccb9dcc31b7f27897b3f70755ca60396a28ebdc6
Alfred Shaw <alfred.shaw@hotmail.co.uk>
2023-12-14 16:04:56 +0000

    Ready for Heroku

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose'
 import 'dotenv/config'
 import router from './config/routes.js'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
 
@@ -13,6 +18,12 @@ app.use((req, res, next) => {
 })
 
 app.use('/api', router)
+
+app.use(express.static(path.join(__dirname, 'client', 'dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 async function startServer(){
   try {
